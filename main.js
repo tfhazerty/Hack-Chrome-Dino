ffcc3903f4c435a4d07cdcc9ce03fffaafe1ab91
tfhazerty <64971565+tfhazerty@users.noreply.github.com>
2022-01-27 18:05:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
     Star the repository please 
 */
 let signe = prompt("Puts the speed that you would like");
+alert("Well done, you hack chrome dino game");
 var original = Runner.prototype.gameOver;
 Runner.prototype.gameOver = function (){};
 Runner.instance_.setSpeed(signe);
